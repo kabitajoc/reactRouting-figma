f329c5e330e927ddfea183e8008d392bf803543f
Kabita Joshi <kabitajoshi49@gmail.com>
2023-08-17 14:48:20 +0545

    redux-login-form completed

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./Login.css";
-// import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signin } from "../../redux-login/userSlice";
+
 function Login() {
   const [formData, setFormData] = useState({
     email: "",
@@ -13,61 +13,19 @@ function Login() {
     password: "",
   });
   const dispatch = useDispatch();
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }));
-
-    // const handleInput = (event) => {
-    //     const name = event.target.name;
-    //     const value = event.target.value;
-
-    //     setInput((Input) => ({ ...Input, [name]: value }));
-    //   };
   };
-  const handleError = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
 
-    if (name === "password") {
-      if (value.length < 10) {
-        // || value.length > 10
-        setErrors((prevErrors) => ({
-          ...prevErrors,
-          [name]: "Password should be of 10 characters",
-        }));
-      } else if (value.includes("_")) {
-        setErrors((prevErrors) => ({
-          ...prevErrors,
-          [name]: "You cannot use an underscore",
-        }));
-      } else {
-        setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
-      }
-    }
-
-    if (name === "email") {
-      if (!value.includes("@")) {
-        setErrors((prevErrors) => ({
-          ...prevErrors,
-          [name]: "Email must contain '@'",
-        }));
-      } else {
-        setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
-      }
-    }
-  };
   const handleSubmit = (e) => {
-    e.preventDefault;
-    dispatch(
-      signin({
-        email: formData.email,
-        password: formData.password,
-        signedIn: true,
-      })
-    );
+    e.preventDefault();
+
+    dispatch(signin({ email: formData.email, password: formData.password }));
   };
 
   return (
@@ -76,22 +34,17 @@ function Login() {
         <h2>Login form</h2>
         <div className="email">
           <label htmlFor="email">Email</label>
-          <br></br>
+          <br />
           <input
             type="text"
             name="email"
-            placeholder=" Enter email"
+            placeholder="Enter email"
             value={formData.email}
-            onBlur={handleError}
             onChange={handleChange}
           />
-          <p className="error">{errors.email}</p>
         </div>
         <br />
-        <div
-          className="password
-        "
-        >
+        <div className="password">
           <label htmlFor="password">Password</label>
           <br />
           <input
@@ -99,10 +52,8 @@ function Login() {
             placeholder="********"
             name="password"
             value={formData.password}
-            onBlur={handleError}
             onChange={handleChange}
           />
-          <p className="error">{errors.password}</p>
         </div>
 
         <button type="submit">Sign in</button>
--- src/redux-login/userSlice.jsx
+++ src/redux-login/userSlice.jsx
@@ -1,16 +1,32 @@
-
 import { createSlice } from "@reduxjs/toolkit";
+
+const validCredentials = [
+  { email: "admin@gmail.com", password: "admin@12345" },
+  
+];
+
 export const userSlice = createSlice({
   name: "user",
   initialState: {
     user: null,
+    isSignedIn: false,
   },
   reducers: {
     signin: (state, action) => {
-      state.user = action.payload;
+      const matchedCredential = validCredentials.find(
+        (credential) =>
+          credential.email === action.payload.email &&
+          credential.password === action.payload.password
+      );
+
+      if (matchedCredential) {
+        state.user = action.payload;
+        state.isSignedIn = true;
+      }
     },
   },
 });
+
 export const { signin } = userSlice.actions;
 export const selectUser = (state) => state.user.user;
 export default userSlice.reducer;
